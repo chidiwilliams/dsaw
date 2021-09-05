92dbff7bf75ac315f7a58be59251fa5facd5a68f
Chidi Williams <williamschidi1@gmail.com>
2021-09-05 12:03:22 +0100

    Add comments

--- quadtrees/quadtree.js
+++ quadtrees/quadtree.js
@@ -1,16 +1,45 @@
+/**
+ * A Point holds (x,y) coordinates
+ *
+ * @class Point
+ */
 class Point {
-  constructor(x = 0, y = 0) {
+  /**
+   * Creates an instance of Point.
+   * @param {number} x
+   * @param {number} y
+   * @memberof Point
+   */
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 }
 
+/**
+ * A Boundary is an enclosed rectangular area.
+ *
+ * @class Boundary
+ */
 class Boundary {
+  /**
+   * Creates an instance of Boundary.
+   * @param {Point} topLeft The top-left corner of the Boundary
+   * @param {Point} bottomRight The bottom-right corner of the Boundary
+   * @memberof Boundary
+   */
   constructor(topLeft, bottomRight) {
     this.topLeft = topLeft;
     this.bottomRight = bottomRight;
   }
 
+  /**
+   * Returns true if the given point is within the Boundary
+   *
+   * @param {Point} point
+   * @returns
+   * @memberof Boundary
+   */
   contains(point) {
     return (
       point.x >= this.topLeft.x &&
@@ -20,6 +49,13 @@ class Boundary {
     );
   }
 
+  /**
+   * Returns true if the given boundary intersects with this boundary
+   *
+   * @param {Boundary} boundary
+   * @returns
+   * @memberof Boundary
+   */
   intersects(boundary) {
     return (
       // not too right
@@ -34,49 +70,132 @@ class Boundary {
   }
 }
 
-class Node {
+/**
+ * An Element contains some data and its position in space
+ *
+ * @class Element
+ */
+class Element {
+  /**
+   * Creates an instance of Element.
+   *
+   * @param {Point} position
+   * @param {any} data
+   * @memberof Element
+   */
   constructor(position, data) {
     this.position = position;
     this.data = data;
   }
 }
 
+/**
+ * A Quadtree
+ *
+ * @class Quadtree
+ */
 class Quadtree {
-  static CAPACITY = 4;
+  /**
+   * Maximum number of elements that can be held in this Quadtree node
+   * @static
+   * @memberof Quadtree
+   */
+  static NODE_CAPACITY = 4;
 
-  nodes = [];
+  /**
+   * Elements held in this Quadtree node
+   * @type {Element[]}
+   * @memberof Quadtree
+   */
+  elements = [];
+
+  /**
+   * Child node in thje top-left corner
+   * @type Quadtree
+   * @memberof Quadtree
+   */
   topLeftTree = null;
+
+  /**
+   * Child node in thje top-right corner
+   * @type Quadtree
+   * @memberof Quadtree
+   */
   topRightTree = null;
+
+  /**
+   * Child node in thje bottom-left corner
+   * @type Quadtree
+   * @memberof Quadtree
+   */
   bottomLeftTree = null;
+
+  /**
+   * Child node in thje bottom-right corner
+   * @type Quadtree
+   * @memberof Quadtree
+   */
   bottomRightTree = null;
 
+  /**
+   * Creates an instance of Quadtree.
+   * @param {Boundary} boundary
+   * @memberof Quadtree
+   */
   constructor(boundary) {
     this.boundary = boundary;
   }
 
-  insert(node) {
-    if (!this.boundary.contains(node.position)) {
+  /**
+   * Inserts an element into the Quadtree node. If the node is already at its maximum
+   * capacity, the node will first be subdivided into four child nodes. Then, the new
+   * element will be added to the child node it fits into.
+   *
+   * @param {Element} element
+   * @returns true if the element has been inserted into this node or one of its child nodes
+   * @memberof Quadtree
+   */
+  insert(element) {
+    if (!this.boundary.contains(element.position)) {
       return false;
     }
 
-    if (this.nodes.length < Quadtree.CAPACITY && !this.topLeftTree) {
-      this.nodes.push(node);
+    // If this node has not yet reached its capacity and has not
+    // yet been subdivided, insert the element into this node
+    if (this.elements.length < Quadtree.NODE_CAPACITY && !this.topLeftTree) {
+      this.elements.push(element);
       return true;
     }
 
+    // At this point, the node has either already been subdivided,
+    // or has reached its capacity but hasn't been subdivided
+
+    // If the node has reached its capacity,
+    // but hasn't been subdivided, subdivide
     if (!this.topLeftTree) {
       this.subdivide();
     }
 
-    if (this.topLeftTree.insert(node)) return true;
-    if (this.bottomLeftTree.insert(node)) return true;
-    if (this.topRightTree.insert(node)) return true;
-    if (this.bottomRightTree.insert(node)) return true;
+    // Insert the element into its correct child node. We can try inserting into all the sub-trees
+    // The wrong ones (where the element's position is outside the sub-tree's boundary) would
+    // simply return false, until we find the correct sub-tree.
+    if (this.topLeftTree.insert(element)) return true;
+    if (this.bottomLeftTree.insert(element)) return true;
+    if (this.topRightTree.insert(element)) return true;
+    if (this.bottomRightTree.insert(element)) return true;
+
+    // We shouldn't ever get to this point
     return false;
   }
 
+  /**
+   * Breaks this node into four child nodes and moves the
+   * elements in the node into their correct child nodes.
+   *
+   * @memberof Quadtree
+   */
   subdivide() {
-    // Create sub-trees
+    // Create the four sub-trees
     const { topLeft, bottomRight } = this.boundary;
     const midPoint = new Point((topLeft.x + bottomRight.x) / 2, (topLeft.y + bottomRight.y) / 2);
     this.topLeftTree = new Quadtree(
@@ -92,25 +211,43 @@ class Quadtree {
       new Boundary(new Point(midPoint.x, midPoint.y), new Point(bottomRight.x, bottomRight.y))
     );
 
-    // Move existing nodes to sub-trees
-    this.nodes.forEach((node) => {
-      if (this.topLeftTree.insert(node)) return;
-      if (this.bottomLeftTree.insert(node)) return;
-      if (this.topRightTree.insert(node)) return;
-      if (this.bottomRightTree.insert(node)) return;
+    // Move the elements in the node to the child node that should contain the element.
+    // Again, we can try inserting each element into all the sub-trees. The wrong ones
+    // (where the element's position is outside the sub-tree's boundary) would simply
+    // return false, until we find the correct sub-tree.
+    this.elements.forEach((element) => {
+      if (this.topLeftTree.insert(element)) return;
+      if (this.bottomLeftTree.insert(element)) return;
+      if (this.topRightTree.insert(element)) return;
+      if (this.bottomRightTree.insert(element)) return;
     });
-    this.nodes = [];
+
+    // We no longer need to keep the elements in this node
+    this.elements = [];
   }
 
+  /**
+   * Returns all the elements within the given boundary
+   *
+   * @param {Boundary} searchBoundary
+   * @returns {Element[]}
+   * @memberof Quadtree
+   */
   search(searchBoundary) {
+    // If this node does not intersect with the search boundary, the nodes
+    // (and all its child nodes) do not have any elements to offer the query
     if (!this.boundary.intersects(searchBoundary)) {
       return [];
     }
 
+    // If this node has not yet been subdivided, return
+    // all the elements within the search boundary
     if (!this.topLeftTree) {
-      return this.nodes.filter((node) => searchBoundary.contains(node.position));
+      return this.elements.filter((node) => searchBoundary.contains(node.position));
     }
 
+    // If the node has been subdivided, search all
+    // the child nodes and merge the results
     return this.topLeftTree
       .search(searchBoundary)
       .concat(this.bottomLeftTree.search(searchBoundary))
@@ -119,4 +256,4 @@ class Quadtree {
   }
 }
 
-module.exports = { Quadtree, Node, Point, Boundary };
+module.exports = { Quadtree, Element, Point, Boundary };
--- quadtrees/quadtree.test.js
+++ quadtrees/quadtree.test.js
@@ -1,20 +1,18 @@
-const { Node, Point, Quadtree, Boundary } = require('./quadtree');
+const { Element, Point, Quadtree, Boundary } = require('./quadtree');
 const assert = require('assert');
 
-{
-  console.log('Quadtree - returns the searched nodes');
-  const tree = new Quadtree(new Boundary(new Point(0, 0), new Point(8, 8)));
+console.log('Quadtree - returns the searched nodes');
+const tree = new Quadtree(new Boundary(new Point(0, 0), new Point(8, 8)));
 
-  const n1 = new Node(new Point(1, 1), '1');
-  const n2 = new Node(new Point(2, 2), '2');
-  const n3 = new Node(new Point(4, 4), '2');
-  const n4 = new Node(new Point(6, 6), '4');
+const n1 = new Element(new Point(1, 1), '1');
+const n2 = new Element(new Point(2, 2), '2');
+const n3 = new Element(new Point(4, 4), '2');
+const n4 = new Element(new Point(6, 6), '4');
 
-  tree.insert(n1);
-  tree.insert(n2);
-  tree.insert(n3);
-  tree.insert(n4);
+tree.insert(n1);
+tree.insert(n2);
+tree.insert(n3);
+tree.insert(n4);
 
-  assert.deepStrictEqual(tree.search(new Boundary(new Point(3, 3), new Point(5, 5))), [n3]);
-  assert.deepStrictEqual(tree.search(new Boundary(new Point(3, 3), new Point(7, 7))), [n3, n4]);
-}
+assert.deepStrictEqual(tree.search(new Boundary(new Point(3, 3), new Point(5, 5))), [n3]);
+assert.deepStrictEqual(tree.search(new Boundary(new Point(3, 3), new Point(7, 7))), [n3, n4]);
